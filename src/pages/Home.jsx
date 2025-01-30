81e423e97a00c3ab7ef2bfa4db87b62a9779fec3
Rohandede1825 <rohandede97@gmail.com>
2025-01-30 20:04:49 +0530

    Add loading state to Home component and update button text during API call

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ const Home = () => {
   const [tempLimit, setTempLimit] = useState(30);
   const [currentTemp, setCurrentTemp] = useState(null);
   const [history, setHistory] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   // Handle input change
   const handleChange = (e) => {
@@ -27,19 +28,12 @@ const Home = () => {
     }
 
     try {
-      const url = "https://temp-app-backend.onrender.com/api/temp/add"
-      const response = await fetch(url, {
+      const backendUrl = "https://temp-app-backend.onrender.com/api/temp/add"
+      const response = await fetch(backendUrl, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ temperature, tempLimit })
       });
-
-
-      console.log(url);
-      console.log(response);
-      
-      
-    
 
 
       if (response.ok) {
@@ -73,8 +67,9 @@ const Home = () => {
             placeholder='Set Temperature Limit'
             className='border border-gray-300 p-2 rounded w-full mb-4'
           />
-          <button type='submit' className='bg-gray-800 text-white px-4 py-2 rounded-lg'>
-            Add Temperature
+          <button disabled={loading} type='submit' className='bg-gray-800 text-white px-4 py-2 rounded-lg'>
+            {/* Add Temperature */}
+            {loading ? 'Adding Temperature...' : 'Add Temperature'}
           </button>
         </form>
       </motion.div>
